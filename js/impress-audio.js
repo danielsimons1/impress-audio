ee3c8049e043f2f7c021bdfaed6ea74942e9def4
danielsimons1 <daniel.simons1@gmail.com>
2013-06-21 15:05:59 -0500

    Adding play to impress prototype

--- js/impress-audio.js
+++ js/impress-audio.js
@@ -1,11 +1,28 @@
 (function(document, $, undefined) {
 	'use strict';
 
-	$(document).on('impress:stepenter', function(event,item) {
-		var audio = $(event.target).find('audio')[0];
+	var isPlaying;
+
+	$(document).on('impress:stepenter', function(event) {
+		var $currSlide = $(event.target),
+			audio = $currSlide.find('audio')[0];
 		if(audio) {
 			audio.play();
+			if(isPlaying && $currSlide[0] != $currSlide.parent().children().last()[0]) {
+				audio.addEventListener('ended',function() {
+					impress().goto($currSlide.next());
+					audio.removeEventListener('ended');
+					
+				});
+			} else {
+				isPlaying = false;
+			}
+			
 		}
 	});
 
+	impress().play = function() {
+		isPlaying = true;
+		this.goto(0);
+	};
 })(document, jQuery);
